669541e2f39314a16928684d4476a7340659c210
mdctenga <mdctenga@gmail.com>
2015-08-29 18:51:13 -1000

    Create an injectable and configurable Provider

--- public/js/myController.controller.js
+++ public/js/myController.controller.js
@@ -2,11 +2,12 @@
 (function() {
   angular
     .module('myApp')
-    .controller('myController', ['$scope', 'myAppTitle', 'CharacterVersionFactory', 'BookService', function ($scope, myAppTitle, CharacterVersionFactory, BookService){
+    .controller('myController', ['$scope', 'myAppTitle', 'CharacterVersionFactory', 'BookService', 'Movies', function ($scope, myAppTitle, CharacterVersionFactory, BookService, Movies){
       $scope.myModel = "hello";
       $scope.title = myAppTitle;
       $scope.characters = CharacterVersionFactory;
       $scope.books = BookService.getBooks();
       $scope.addBook = BookService.addBook;
+      $scope.movies = Movies.query();
     }]);
 })();
--- server.js
+++ server.js
@@ -3,11 +3,18 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.get('/', function(req, res){
-  res.send('Hello World');
+app.get('/api', function(req, res){
+  var movies = [
+    {title: "Avengers"},
+    {title: "Avengers: Age of Utron"},
+    {title: "Captain Americal: Civil War"}
+  ];
+  res.send(movies);
 });
 
 var server = app.listen(3000, function(){
   var host = server.address().address;
   var port = server.address().port;
 });
+
+//set up an array of movies
