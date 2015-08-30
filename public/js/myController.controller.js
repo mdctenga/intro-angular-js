154146bedffdbac9aa984e01de26b038db401da1
mdctenga <mdctenga@gmail.com>
2015-08-29 15:49:32 -1000

    Create injectable Factory

--- public/js/myController.controller.js
+++ public/js/myController.controller.js
@@ -2,8 +2,9 @@
 (function() {
   angular
     .module('myApp')
-    .controller('myController', ['$scope', 'myAppTitle', 'APP_VERSION', function ($scope, myAppTitle){
+    .controller('myController', ['$scope', 'myAppTitle', 'CharacterVersionFactory', function ($scope, myAppTitle, CharacterVersionFactory){
       $scope.myModel = "hello";
       $scope.title = myAppTitle;
+      $scope.characters = CharacterVersionFactory;
     }]);
 })();
